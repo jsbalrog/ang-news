7d92d4ed2abe9ffefa5cebb752b88dff9d1c8b4c
Ted Jenkins <edwardtjenkins@gmail.com>
2014-10-22 17:27:03 -0600

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+/* global app:true */
+/* exported app */
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+  .module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
